d552a11023800df18fa206bce87ac79fba1b71fa
Riyaannn <riyaannedu@gmail.com>
2026-07-14 11:09:59 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int min=0;
+        for(int i=0;i<prices.length-1;i++){
+            for(int j=i+1;j<prices.length;j++){
+                if(prices[j]>=prices[i]){
+                int diff=prices[j]-prices[i];
+                min=Math.max(diff,min);
+            }
+            }
+        }
+        return min;
+    }
+}
