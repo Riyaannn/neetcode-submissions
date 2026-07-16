c5241f2359d5bcd3afe60aed316c3cfe99c425bb
Riyaannn <riyaannedu@gmail.com>
2026-07-16 12:47:42 +0530

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean isValid(String s) {
+        Deque<Character> stack=new ArrayDeque<>();
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+        
+        if(ch=='('||ch=='['||ch=='{'){
+            stack.push(ch);
+        }
+        else{
+            if(stack.isEmpty()){
+                return false;
+            }
+            
+               if (ch=='}' && stack.peek() == '{') {
+                stack.pop();
+                } 
+                else if (ch==')' && stack.peek() == '(') {
+                stack.pop();
+                } 
+                else if (ch==']' && stack.peek() == '[') {
+                stack.pop();
+                } 
+                else {
+                    return false;
+                }
+            
+        }
+        }
+        return stack.isEmpty();
+        
+    }
+}
